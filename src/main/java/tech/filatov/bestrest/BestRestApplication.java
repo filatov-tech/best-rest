5c9d9a7cc03bcea7db7bceb9441370213e8b5590
filatov-tech <filatov.dd@yandex.ru>
2022-04-27 16:57:35 +0300

    Init

---
+++ src/main/java/tech/filatov/bestrest/BestRestApplication.java
@@ -0,0 +1,13 @@
+package tech.filatov.bestrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/filatov/bestrest/BestRestApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.filatov.bestrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
